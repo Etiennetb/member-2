0f492b3888b47f851cf9fec2260beec97ea8e22f
Etienne Bouron <etienne.toutbien@gmail.com>
2017-10-31 12:55:20 +0100

    implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,28 @@
 class UsersController < ApplicationController
 	
-	def index
-		@users = User.all 
-	end
+	 def show
+    @user = User.find(params[:id])
+  end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		@user.save
-		if @user.save
-		redirect_to @user
-		else
-			render "new"
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+  private
 
-	def destroy
-		@user = User.find(params[:id])
-		@user.destroy 
-			redirect_to users_path
-	end
- 
-	def update
-		@user = User.find(params[:id])
-		if @user.update(user_params)
-			redirect_to @user
-		else
-			render "edit"
-		end
-	end
-
-	def show
-		@user = User.find(params[:id])
-		puts "Ceci est mon user : #{@user}"
-	end
-
-	private
-
-	def user_params
-		params.permit(:name, :email)
-	end
-
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(name: "Alix", email: "alix.hero@rome.com")
+user1 = User.create(name: "Alix", email: "alix.hero@rome.com", password: "romealix", password_confirmation: "romealix")
